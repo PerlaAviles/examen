cd5be1a42f9eb3b7cef2c40bed689863df4d593e
PerlaAviles <155588328+PerlaAviles@users.noreply.github.com>
2024-01-29 11:33:14 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
-// alert("Hola mundo");
-//se manda un mensaje al usuario que esta trabajando 
-let ip = document.getElementById("ip");
-let pais = document.getElementById("pais");
-let continente = document.getElementById("continente");
-let zona_horaria = document.getElementById("zona_horaria");
+//alert ("Hola mundo");
+
+let ip=document.getElementById("ip");
+let pais=document.getElementById("pais");
+let continente=document.getElementById("continente");
+let zona_horaria=document.getElementById("zona_horaria");
+
 const solicitudAPI = () => {
-
-    // Hacer una petición para un usuario con ID especifico
     axios.get("https://itp-bdd-ptag.000webhostapp.com/php-geoip-api/index.php")
-        .then(function (response) { // se crea una funcion en el cual recibo como parametro la funcion respuesta
-            // manejar respuesta exitosa
-            console.log(response.data.pais);
-            ip.innerHTML = response.data.ip;
-            pais.innerHTML = response.data.pais;
-            continente.innerHTML = response.data.continente;
-            zona_horaria.innerHTML = response.data.zona_horaria;
+    .then(function (response) {
+      // handle success
+      console.log(response.data.ip);
+      ip.innerHTML = response.data.ip;
+      pais.innerHTML = response.data.pais;
+      continente.innerHTML = response.data.continente;
+      zona_horaria.innerHTML = response.data.zona_horaria;
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+     
+  };
 
-        })
-        .catch(function (error) {
-            // manejar error
-            console.log(error);
-        })
-        .finally(function () {
-            // siempre sera executado
-        });
-};
-
+  //Llama al evento LOAD cada vez que se actualiza la pag
+  //Y llama a la funcion solicitudAPI que tiene la rutina de llamar a la API desde Axios
+  window.addEventListener('load', solicitudAPI);
