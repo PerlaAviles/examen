90be6eaf42ff7f4aa9c44bd077265b1abe5cb299
PerlaAviles <155588328+PerlaAviles@users.noreply.github.com>
2024-01-29 16:28:20 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -24,6 +24,31 @@ const solicitudAPI = () => {
     });
      
   };
+  // Función para manejar el envío del formulario de registro de productos
+document.getElementById('Productos').addEventListener('submit', function (event) {
+    event.preventDefault(); // Evitar que el formulario se envíe por defecto
+
+    // Obtener los valores del formulario
+    var productName = document.getElementById('Producto').value;
+    var productPrice = document.getElementById('Precio').value;
+    var productStock = document.getElementById('Stock').value;
+
+    // Agregar la fila a la tabla de productos
+    var newRow = document.createElement('tr');
+    newRow.innerHTML = `
+        <td>${Producto}</td>
+        <td>${Precio}</td>
+        <td>${Stock}</td>
+    `;
+    document.getElementById('producto').appendChild(newRow);
+
+    // Mostrar la tabla de productos
+    document.getElementById('producto').style.display = 'block';
+});
+
+// Llama al evento LOAD CADA VEZ QUE SE REFRESCA O ACTUALIZA LA PÁGINA 
+// Y llama a la funcion solicitudAPI que tiene la rutina de llamar a la API desde axios
+window.addEventListener('load', solicitudAPI);
 
   //Llama al evento LOAD cada vez que se actualiza la pag
   //Y llama a la funcion solicitudAPI que tiene la rutina de llamar a la API desde Axios
